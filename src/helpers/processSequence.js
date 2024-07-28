3ff1a280511952ff68969a38ce0d5a880dfb143f
BulBat <bulatkhayrullintermin@mail.ru>
2024-07-28 22:54:30 +0300

    first and finish commit

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,77 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from "../tools/api";
+import * as ramda from "ramda";
+import { pipe, lt, gt } from "ramda";
+const api = new Api();
 
- const api = new Api();
+//валидация
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const isLessThan10 = pipe(ramda.split(""), ramda.length, gt(10));
+const isMoreThan2 = pipe(ramda.split(""), ramda.length, lt(2));
+const isPositiveNum = pipe(ramda.startsWith("-"), ramda.not);
+const isNumber = pipe(ramda.add(0), ramda.type, ramda.equals("Number"));
+const validate = ramda.allPass([
+  isLessThan10,
+  isMoreThan2,
+  isPositiveNum,
+  isNumber,
+]);
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+//преобразования
+const toNumber = ramda.pipe(parseFloat, Math.round);
+const square = (value) => value ** 2;
+const modulo3 = (num) => ramda.modulo(num, 3);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const params = (number) => ({
+  from: 10,
+  to: 2,
+  number,
+});
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const getNumber = (number) =>
+  api.get("https://api.tech/numbers/base", params(number));
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const getAnimal = (id) => api.get(`https://animals.tech/${id}`, id);
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  const tapWriteLog = (val) => ramda.tap(writeLog, val);
+  const handleValidationError = () => handleError("ValidationError");
+
+  const processGetAnimal = ramda.pipe(
+    tapWriteLog,
+    ramda.length,
+    tapWriteLog,
+    square,
+    tapWriteLog,
+    modulo3,
+    tapWriteLog,
+    getAnimal,
+    ramda.otherwise(handleError),
+    ramda.andThen(
+      ramda.when(ramda.has("result"), pipe(ramda.prop("result"), handleSuccess))
+    )
+  );
+
+  const processRequests = pipe(
+    toNumber,
+    getNumber,
+    ramda.otherwise(handleError),
+    ramda.andThen(
+      ramda.when(
+        ramda.has("result"),
+        pipe(ramda.prop("result"), processGetAnimal)
+      )
+    )
+  );
+
+  const processValue = ramda.pipe(
+    tapWriteLog,
+    ramda.ifElse(validate, processRequests, handleValidationError)
+  );
+
+  processValue(value);
+};
 
 export default processSequence;
